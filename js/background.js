1ae36b8e5375591a35dfa72b1ccba1fd370f48d0
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2024-01-13 12:36:00 +0100

    changes from swopl's fork

--- js/background.js
+++ js/background.js
@@ -6,18 +6,58 @@ fetch("https://api.invidious.io/instances.json")
 	.then((resp) => resp.json())
 	.then((array) => array.forEach((json) => INVIDIOUS_INSTANCES.push(json[0])));
 
+const redirectHostDefault = "piped.video";
+let redirectHost = redirectHostDefault;
+
+browser.storage.sync.get(["redirectHost"], (result) => {
+	redirectHost = result.redirectHost || redirectHostDefault;
+	if (!result.redirectHost) {
+		browser.storage.sync.set({ redirectHost });
+	}
+});
+
+browser.storage.onChanged.addListener((changes) => {
+	redirectHost = changes?.redirectHost?.newValue || redirectHostDefault;
+	if (!changes.redirectHost.newValue) {
+		browser.storage.sync.set({ redirectHost: redirectHostDefault });
+	}
+});
+
 browser.webRequest.onBeforeRequest.addListener(
 	(details) => {
 		const url = new URL(details.url);
 		if (url.hostname.endsWith("youtu.be") && url.pathname.length > 1) {
-			return { redirectUrl: "https://piped.kavin.rocks/watch?v=" + url.pathname.substr(1) };
+			return { redirectUrl: `https://${redirectHost}/watch?v=${url.pathname.substr(1)}` };
 		}
 		if (
 			url.hostname.endsWith("youtube.com") ||
 			url.hostname.endsWith("youtube-nocookie.com") ||
 			INVIDIOUS_INSTANCES.includes(url.hostname)
 		) {
-			url.hostname = "piped.kavin.rocks";
+			// TODO: write in a cleaner way
+			if (url.pathname.endsWith("/feed/trending")) {
+				url.pathname = url.pathname.replace(/\/feed\/trending$/, "/trending");
+			}
+			if (url.pathname.endsWith("/featured")) {
+				url.pathname = url.pathname.replace(/\/featured$/, "");
+			}
+			if (url.pathname.endsWith("/videos")) {
+				url.pathname = url.pathname.replace(/\/videos$/, "");
+				url.searchParams.set("tab", "videos");
+			}
+			if (url.pathname.endsWith("/shorts")) {
+				url.pathname = url.pathname.replace(/\/shorts$/, "");
+				url.searchParams.set("tab", "shorts");
+			}
+			if (url.pathname.endsWith("/streams")) {
+				url.pathname = url.pathname.replace(/\/streams$/, "");
+				url.searchParams.set("tab", "livestreams");
+			}
+			if (url.pathname.endsWith("/playlists")) {
+				url.pathname = url.pathname.replace(/\/playlists$/, "");
+				url.searchParams.set("tab", "playlists");
+			}
+			url.hostname = redirectHost;
 			return { redirectUrl: url.href };
 		}
 	},
